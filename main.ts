a18ad3b901619f5c3adefc0705a7216b45265498
AmitHirsh12 <166304780+AmitHirsh12@users.noreply.github.com>
2024-04-07 15:55:08 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let מונה = 5
+for (let index = 0; index < 5; index++) {
+    basic.showNumber(מונה)
+    basic.pause(1000)
+    מונה += -1
+}
+basic.showIcon(IconNames.House)
+basic.forever(function () {
+	
+})
